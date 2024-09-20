6257f0ad2e3e27af7752c0a5c9aa9890c38c3994
MiFedha_MyFedha <sauscser@gmail.com>
2024-09-20 17:29:14 +0300

    Commit 1

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mifedha3": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "mifedha32a5fbe29": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminOneGroupRole": "string",
+      "AdminOneTwoGroupRole": "string",
+      "AdvocateGroupRole": "string",
+      "BizOwnerGroupRole": "string",
+      "BranchAdminGroupRole": "string",
+      "BranchAdminTwoGroupRole": "string",
+      "CountyAdminGroupRole": "string",
+      "CountyAdminTwoGroupRole": "string",
+      "ExecutiveGroupRole": "string",
+      "KFKubwaGroupRole": "string",
+      "KFNdogoGroupRole": "string",
+      "RegionalAdminGroupRole": "string",
+      "RegionalAdminTwoGroupRole": "string",
+      "SpareGrpGroupRole": "string"
+    }
+  }
+}
